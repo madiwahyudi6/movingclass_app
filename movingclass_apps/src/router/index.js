3afae8807c64f156f5e7c36b71ea07060cf7b096
Muhammad Adi <madiwahyudi6@gmail.com>
2019-02-28 19:18:19 +0700

    Tugas Akhir

---
+++ movingclass_apps/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Activities from '@/components/Activities'
+import Studentclass from '@/components/Studentclass'
+import Classroom from '@/components/Classroom'
+import Subject from '@/components/Subject'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/activities',
+      name: 'Activities',
+      component: Activities
+    },
+    {
+      path: '/studentclass',
+      name: 'Studentclass',
+      component: Studentclass
+    },
+    {
+      path: '/classroom',
+      name: 'Classroom',
+      component: Classroom
+    },
+    {
+      path: '/subject',
+      name: 'Subject',
+      component: Subject
+    }
+  ]
+})
